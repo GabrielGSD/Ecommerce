ebded857956da38265459e2cdd8935437d7bd90f
Gabriel de Souza <gabrieldaniel@gec.inatel.br>
2024-08-18 14:41:01 -0300

    feat: Application Services Layer - Implementing contracts and methods

--- src/Ecommerce.Application/CustomerApplicationService.cs
+++ src/Ecommerce.Application/CustomerApplicationService.cs
@@ -4,16 +4,43 @@ public class CustomerApplicationService : ICustomerApplicationService
 {
     private readonly ICustomerService _customerService;
     private readonly IMapper<CustomerDto, Customer> _mapperEntity;
+    private readonly IMapper<Customer, CustomerDto> _mapperDto;
 
-    public CustomerApplicationService(ICustomerService customerService, IMapper<CustomerDto, Customer> mapperEntity)
+    public CustomerApplicationService(ICustomerService customerService, 
+                                    IMapper<CustomerDto, Customer> mapperEntity, 
+                                    IMapper<Customer, CustomerDto> mapperDto)
     {
         _customerService = customerService;
         _mapperEntity = mapperEntity;
+        _mapperDto = mapperDto;
     }
 
     public void SaveCustomer(CustomerDto customerDto)
     {
         var customer = _mapperEntity.Map(customerDto);
         _customerService.SaveCustomer(customer);
+    }
+
+    public void UpdateCustomer(CustomerDto customerDto)
+    {
+        var customerEntity = _mapperEntity.Map(customerDto);
+        _customerService.UpdateCustomer(customerEntity);
+    }
+
+    public void DeleteCustomer(string id)
+    {
+        _customerService.DeleteCustomer(id);
+    }
+
+    public IEnumerable<CustomerDto> GetCustomers()
+    {
+        var customers = _customerService.GetCustomers();
+        return customers.Select(c => _mapperDto.Map(c));
+    }
+
+    public CustomerDto GetCustomerById(string id)
+    {
+        var customer = _customerService.GetCustomerById(id);
+        return _mapperDto.Map(customer);
     }
 }
--- src/Ecommerce.Application/Interfaces/ICustomerApplicationService.cs
+++ src/Ecommerce.Application/Interfaces/ICustomerApplicationService.cs
@@ -3,4 +3,8 @@ namespace Ecommerce.Application.Interfaces;
 public interface ICustomerApplicationService
 {
     void SaveCustomer(CustomerDto customer);
+    void UpdateCustomer(CustomerDto customer);
+    void DeleteCustomer(string id);
+    IEnumerable<CustomerDto> GetCustomers();
+    CustomerDto GetCustomerById(string id);
 }
