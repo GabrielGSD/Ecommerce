2a95a09b9d743959d824a95d09679606eb6d8997
Gabriel de Souza <gabrieldaniel@gec.inatel.br>
2024-08-18 21:53:06 -0300

    feat: Implementing Health Check

--- src/Ecommerce.Api/Program.cs
+++ src/Ecommerce.Api/Program.cs
@@ -4,14 +4,20 @@ using Ecommerce.Infrastructure.CrossCutting.Options;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.Configure<RavenDbSettings>(builder.Configuration.GetSection("RavenDbSettings"));
+var ravenDbSettings = builder.Configuration.GetSection("RavenDbSettings");
+builder.Services.Configure<RavenDbSettings>(ravenDbSettings);
 builder.Services.AddControllers();
 builder.Services.AddRavenDb();
 builder.Services.AddRepositories();
 builder.Services.AddDomainServices();
 builder.Services.AddMappers();
 builder.Services.AddApplicationServices();
-
+builder.Services.AddHealthChecks()
+	.AddRavenDB(setup =>
+	{
+		setup.Database = ravenDbSettings["Database"];
+		setup.Urls = [ravenDbSettings["Url"]];
+	});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -36,5 +42,6 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/health");
 
 app.Run();
